0ac6688684f2a60bdb3169d6b43e49182ff7af35
Matthew Perry <mperry@psi.edu>
2020-01-08 13:59:52 -0700

    Moved to Gopher, renamed

--- code/run_shaddai.sh
+++
@@ -1,7 +0,0 @@
-# Arg 1 is the input file
-# Arg 2 (-u) is optional and will update the PDSFileList.pkl file.
-#   - This will increase run time
-#
-source activate shaddai
-python shaddai.py $1 $2 
-source deactivate shaddai
